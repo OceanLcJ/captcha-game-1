5c915f9d82fcb360dbeb4aa9ba2d144e1c416ce8
arash-1819 <51744028+arash-1819@users.noreply.github.com>
2024-11-16 16:35:48 -0500

    Add files via upload

---
+++ src/components/captcha/Waldo.tsx
@@ -0,0 +1,21 @@
+import SelectBoxes from "./SelectBoxes";
+
+interface Props {
+    setSuccess: (val: boolean) => void;
+    setP: (paragraph: string) => void;
+    setL: (paragraph: string) => void;
+}
+
+function Waldo(props: Props) {
+    const correct = [4, 11, 15];
+    props.setP("Select all the images with");
+    props.setL("Waldos");
+
+    return (
+        <>
+            <SelectBoxes setSuccess={props.setSuccess} img={"Waldo.jpeg"} correct={correct}/>
+        </>
+    );
+}
+
+export default Waldo;
