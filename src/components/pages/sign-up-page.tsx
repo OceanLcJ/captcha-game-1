860055f13d8aa3e7c753ae2496c44a44c9f36f79
NicoBobBison <47168714+NicoBobBison@users.noreply.github.com>
2024-11-16 20:42:54 -0500

    Fixed catastrophic bug

--- src/components/pages/sign-up-page.tsx
+++ src/components/pages/sign-up-page.tsx
@@ -53,7 +53,7 @@ export function SignUpPage(){
     const [password, setPassword] = useState("");
 
     const toggleWindow = () => {
-        //alert("Toggle window");
+        alert("Toggle window");
         setWindow(!window);
     };
 
@@ -76,7 +76,7 @@ export function SignUpPage(){
                 <h2>Create a new username and password, or enter existing credentials.</h2>
                 <div className='credential-textbox'><LabelTextBox onChange={setUsername} value={username} name={"Username"} isPassword={false}></LabelTextBox></div>
                 <div className='credential-textbox'><LabelTextBox onChange={setPassword} value={password} name={"Password"} isPassword={true}></LabelTextBox></div>
-                <div className="login-captcha" onClick={toggleWindow}><CaptchaButton click={toggleWindow}/></div>
+                <div className="login-captcha"><CaptchaButton click={toggleWindow}/></div>
                 <button className="signup-button">
                     Create account
                 </button>
